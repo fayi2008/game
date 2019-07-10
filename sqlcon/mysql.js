5018dab4a3cdbd3d4263aff4a6970189f28f7e3c
fay <20611@etransfar.com>
2019-07-10 18:01:17 +0800

    111233213

--- sqlcon/mysql.js
+++ sqlcon/mysql.js
@@ -4,7 +4,7 @@ let connection = mysql.createConnection({
   host: '116.62.150.180',
   user: 'fay',
   password: 'yang0313',
-  database: 'faywow'
+  database: 'game'
 })
 connection.connect()
 module.exports = {
